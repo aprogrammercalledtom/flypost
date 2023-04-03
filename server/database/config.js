0311cb76fa0318bcc5b0228f3d3ff1f79df3745d
iamtomodea <iamtomodea@gmail.com>
2023-04-03 14:25:43 +0100

    updated sequelize url in config.js

--- server/database/config.js
+++ server/database/config.js
@@ -3,7 +3,7 @@ const path = require('path')
 
 dotenv.config({ path: path.join(__dirname, '..', '..', '.env') })
 
-const url = process.env.DATABASE_URL
+const url = `${process.env.DATABASE_URL}?sslmode=no-verify`
 const dialect = process.env.DATABASE_DIALECT || 'postgres'
 
 const timezone = '+00:00' // UTC
